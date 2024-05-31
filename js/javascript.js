0133690a896a036b636c7471c9a3c714ca2c233b
Kamajji22 <ingmardijt@gmail.com>
2024-05-31 11:25:08 +0200

    na presentatie bug fix

--- js/javascript.js
+++ js/javascript.js
@@ -17,7 +17,14 @@ const warning = document.getElementById("warning");
 const gameOver = document.getElementById("gameOver");
 const gameOverTekst = document.getElementById("gameOverTekst");
 
-const speelOpnieuw = document.getElementById("speelOpnieuw")
+const speelOpnieuw = document.getElementById("speelOpnieuw");
+
+//audio
+const audioOlie = new Audio("sound/olie.wav") //https://freesound.org/people/sapphicrabbit/sounds/724223/ olie drinken soundeffect
+const audioSchroef = new Audio("sound/schroef.wav") //https://freesound.org/people/CallFlan/sounds/495094/ schroeven soundeffect
+const audioStroom = new Audio("sound/stroom.wav") //https://freesound.org/people/The-Sacha-Rush/sounds/657803/ stroom soundeffect
+const audioKortsluiting = new Audio("sound/kortsluiting.wav") //https://freesound.org/people/mad-monkey/sounds/66692/ kortsluiting soundeffect
+const audioExplosie = new Audio("sound/explosie.mp3") //https://freesound.org/people/Timbre/sounds/711326/ explosie soundeffect
 
 //lets 
 //max lets
@@ -27,18 +34,15 @@ let maxSchroef = 100;
 
 let randomGetal;
 
-//audio 
-let audioOlie = new Audio("sound/olie.wav") //https://freesound.org/people/sapphicrabbit/sounds/724223/ olie drinken soundeffect
-let audioSchroef = new Audio("sound/schroef.wav") //https://freesound.org/people/CallFlan/sounds/495094/ schroeven soundeffect
-let audioStroom = new Audio("sound/stroom.wav") //https://freesound.org/people/The-Sacha-Rush/sounds/657803/ stroom soundeffect
-let audioKortsluiting = new Audio("sound/kortsluiting.wav") //https://freesound.org/people/mad-monkey/sounds/66692/ kortsluiting soundeffect
-let audioExplosie = new Audio("sound/explosie.mp3") //https://freesound.org/people/Timbre/sounds/711326/ explosie soundeffect
-
-//robbie voiceclips
+//robbie voiceclips (met hulp van Mariska)
 let voicePlaylist = ["sound/robbieIkBen.mp3", 'sound/robbieOlie.mp3', "sound/robbieSchroeven.mp3", "sound/robbieWilNietOntploffen.mp3"]
 let voiceClip = new Audio()
 
+let robbieBangInterval = setInterval(robbieBang, 1000)
+let explosieInterval = setInterval(explosie, 1000)
+
 //functions
+//olie omlaag laten gaan
 function olieOmlaag() {
     if (maxOlie > 0) {
         maxOlie -= 1;
@@ -46,6 +50,7 @@ function olieOmlaag() {
     olieTekst.textContent = "Olie " + maxOlie + "%"
 }
 
+//olie kunnen voeren als olie onder 95 is (niet 100 want dan gaat de olie boven de honderd)
 function voerOlie() {
     if (maxOlie < 95) {
         maxOlie += 5;
@@ -53,6 +58,7 @@ function voerOlie() {
     audioOlie.play()
 }
 
+//schroef omlaag laten gaan
 function schroefOmlaag() {
     if (maxSchroef > 0) {
         maxSchroef -= 1;
@@ -60,6 +66,7 @@ function schroefOmlaag() {
     schroefTekst.textContent = "Schroef " + maxSchroef + "%"
 }
 
+//schroef kunnen voeren als schroef onder 95 is (niet 100 want dan gaat de schroef boven de honderd)
 function voerSchroef() {
     if (maxSchroef < 95) {
         maxSchroef += 5;
@@ -67,6 +74,7 @@ function voerSchroef() {
     audioSchroef.play()
 }
 
+//stroom omlaag laten gaan
 function stroomOmlaag() {
     if (maxStroom > 0) {
         maxStroom -= 1;
@@ -75,6 +83,7 @@ function stroomOmlaag() {
     stroomTekst.textContent = "Stroom " + maxStroom + "%"
 }
 
+//stroom kunnen voeren als stroom onder 95 is (niet 100 want dan gaat de stroom boven de honderd)
 function voerStroom() {
     if (maxStroom < 95) {
         maxStroom += 5;
@@ -82,6 +91,7 @@ function voerStroom() {
     audioStroom.play()
 }
 
+//als de stroom of schroef of stroom onder de 25% is, word robbie bang en speelt er een alarm en komt er een warning 
 function robbieBang() {
     if (maxOlie < 25 || maxSchroef < 25 || maxStroom < 25) {
         robbie.src = "images/robbieBang.gif"
@@ -124,15 +134,14 @@ function playAgain() {
     location.reload();
 }
 
-olieKnop.addEventListener("click", voerOlie)
-schroefKnop.addEventListener("click", voerSchroef)
-stroomKnop.addEventListener("click", voerStroom)
-robbie.addEventListener("click", playRandomRobbie)
+//intervals
+setInterval(olieOmlaag, 1000);
+setInterval(schroefOmlaag, 1500);
+setInterval(stroomOmlaag, 750);
 
-setInterval(olieOmlaag, 1000)
-setInterval(schroefOmlaag, 1000)
-setInterval(stroomOmlaag, 1000)
-let robbieBangInterval = setInterval(robbieBang, 1000)
-let explosieInterval = setInterval(explosie, 1000)
-
-speelOpnieuw.addEventListener("click", playAgain)
+//event listeners
+olieKnop.addEventListener("click", voerOlie);
+schroefKnop.addEventListener("click", voerSchroef);
+stroomKnop.addEventListener("click", voerStroom);
+robbie.addEventListener("click", playRandomRobbie);
+speelOpnieuw.addEventListener("click", playAgain);
